0423e21bf643b06765e4fa91867a5f80780836ff
terabyte01 <so.timothy1@gmail.com>
2017-04-25 18:17:21 -0400

    Update fileLab2Q3.py

--- fileLab2Q3.py
+++ fileLab2Q3.py
@@ -1,12 +1,12 @@
 wordx = raw_input("word: ")
-txtfile = raw_input("read: ")
+txtfile = raw_input("read: ").lower()
 txt = open(txtfile, 'r')
 
+newtxt = map(lambda s: s.strip('!?.,;:'), txt.read().split())
 output = 0
 
-for line in txt:
-    for word in line.split():
-        if wordx == word:
-            output += 1
+for word in newtxt:
+    if word.lower() == wordx:
+        output += 1
 
 print output
