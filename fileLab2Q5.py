8fb52f842a489f4abb462c6f1484bd48d70de730
terabyte01 <so.timothy1@gmail.com>
2017-05-09 22:47:11 -0400

    Update fileLab2Q5.py

--- fileLab2Q5.py
+++ fileLab2Q5.py
@@ -6,17 +6,10 @@ txt.close()
 
 txt = open(txtfile, 'w')
 
-'''
 coding = {' ': '*', 'a': 'u', 'e': 'i'}
 
 for k, v in coding.items():
     newtxt = newtxt.replace(k, v)
-'''
-
-coding = [[' ','*'],['a','u'],['e','i']]
-
-for code in coding:
-    newtxt = newtxt.replace(code[0], code[1])
 
 txt.write(newtxt)
 txt.close()
