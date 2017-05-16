114337a0984ab01d1c04119f6719e2f582123fda
terabyte01 <so.timothy1@gmail.com>
2017-05-15 23:09:03 -0400

    Update pellNumbers.py

--- pellNumbers.py
+++ pellNumbers.py
@@ -5,9 +5,6 @@ def pelli(n):
         return 1
     return 2 * pelli(n - 1) + pelli(n - 2)
 
-def pelliSeq(n):
-    return map(pelli, range(1, n + 1))
-
 L = [0, 1]
 def pelliDynamic(n):
     if n < len(L):
@@ -15,10 +12,5 @@ def pelliDynamic(n):
     L.append(2 * pelliDynamic(n - 1) + pelliDynamic(n - 2))
     return L[n]
 
-def pelliDynamicSeq(n):
-    pelliDynamic(n)
-    return L[:n]
-
 n = int(raw_input(": "))
-#print pelliSeq(n)
-print pelliDynamicSeq(n)
+print pelliSeq(n)
