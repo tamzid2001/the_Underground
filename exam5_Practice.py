d5da8080531598a6105e395aaed76cd93406c349
terabyte01 <so.timothy1@gmail.com>
2017-05-18 22:58:17 -0400

    Update exam5_Practice.py

--- exam5_Practice.py
+++ exam5_Practice.py
@@ -1,12 +1,13 @@
+#PART 2
 def hasUniqueValues(d):
     return len(set(d.values())) == len(d.values())
 
+#PART 3
 def addReverseDictionary(d):
-    d2 = dict()
-    for key in d:
-        d2[d[key]] = key
-    d.update(d2)
+    for key in d.keys():
+        d[d[key]] = key
 
+#PART 4
 import random
 import copy
 pref = dict()
@@ -17,20 +18,23 @@ for name in names:
    random.shuffle(L)
    pref[name ] = L
 
+#a.
 for key in pref:
-    print "Name:", key
-    for school in pref[key]:
-        print school
+    print key, pref[key]
 
+#b.
 L = [ ]
 for key in pref:
     L.append(pref[key][0])
-print "most preferred school:", max(set(L), key = L.count)
+print "most preferred school:", ','.join(set(filter(lambda x: x == max(set(L), key = L.count), L)))
 
+#c.
 print "Stuy, count:", L.count('Stuy')
 
+#d.
 print "Jill, 2nd pref:", pref['Jill'][1]
 
+#e.
 for key in pref:
-    pref[key].remove(min(set(L), key = L.count))
+    pref[key] = pref[key][:-1]
 print "new dict:", pref
