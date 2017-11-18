89012dba63cb9e8b7c79b11da096abaca9a4210f
Timothy So <so.timothy1@gmail.com>
2017-11-17 21:29:54 -0500

    Update isPalindrome.py

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,10 +1,6 @@
 a = raw_input("string, plz: ")
-reverse = ''
 
-for char in a:
-    reverse = char + reverse
-
-if reverse == a:
+if a[::-1] == a:
     print a, "is a palindrome"
 else:
     print a, "is not a palindrome"
